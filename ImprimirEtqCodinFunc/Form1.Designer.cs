21ce62ba90c521ce40a3fcb4d18bc1a77d78f6f7
AllanPereira20 <designsallan20@gmail.com>
2023-06-19 11:54:15 -0300

    first commit

---
+++ ImprimirEtqCodinFunc/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ImprimirEtqCodinFunc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRegistro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblReg = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.lblCodin = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRegistro
+            // 
+            this.txtRegistro.Location = new System.Drawing.Point(140, 46);
+            this.txtRegistro.Name = "txtRegistro";
+            this.txtRegistro.Size = new System.Drawing.Size(106, 20);
+            this.txtRegistro.TabIndex = 0;
+            this.txtRegistro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRegistro_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Registro:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(72, 103);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(0, 20);
+            this.lblNome.TabIndex = 3;
+            // 
+            // lblReg
+            // 
+            this.lblReg.AutoSize = true;
+            this.lblReg.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReg.ForeColor = System.Drawing.Color.White;
+            this.lblReg.Location = new System.Drawing.Point(86, 136);
+            this.lblReg.Name = "lblReg";
+            this.lblReg.Size = new System.Drawing.Size(0, 20);
+            this.lblReg.TabIndex = 4;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.Transparent;
+            this.btnPesquisar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPesquisar.BackgroundImage")));
+            this.btnPesquisar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPesquisar.FlatAppearance.BorderSize = 0;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(252, 40);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(34, 27);
+            this.btnPesquisar.TabIndex = 5;
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(140, 358);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(106, 36);
+            this.btnImprimir.TabIndex = 6;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // lblCodin
+            // 
+            this.lblCodin.AutoSize = true;
+            this.lblCodin.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodin.ForeColor = System.Drawing.Color.White;
+            this.lblCodin.Location = new System.Drawing.Point(75, 197);
+            this.lblCodin.Name = "lblCodin";
+            this.lblCodin.Size = new System.Drawing.Size(0, 20);
+            this.lblCodin.TabIndex = 7;
+            this.lblCodin.Click += new System.EventHandler(this.lblCodin_Click);
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSituacao.ForeColor = System.Drawing.Color.White;
+            this.lblSituacao.Location = new System.Drawing.Point(74, 167);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(0, 20);
+            this.lblSituacao.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 19);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Codin:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(39, 220);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(302, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(392, 420);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.lblCodin);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.lblReg);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRegistro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRegistro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblReg;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Label lblCodin;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ImprimirEtqCodinFunc/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using RawDataPrint;
+
+namespace ImprimirEtqCodinFunc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnc = new SqlConnection(@"Data Source=brspjam-ap07;Initial Catalog=SGM_ONE;User ID=sa;Password=P@ssw0rd");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dt;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+           
+            if (txtRegistro.Text == "")
+            {
+                MessageBox.Show("É necessário apresentar um registro", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Realize alguma ação caso o campo de texto esteja vazio
+
+               
+
+            }
+
+            try
+            {
+                string pesquisa = txtRegistro.Text;
+
+                string sql = "SELECT FUN_NOME, isNULL(FUN_CODIN,0), FUN_REGISTRO, FUN_STATUS FROM FUNCIONARIO WHERE FUN_REGISTRO = @pesquisa";
+
+                SqlCommand cmd = new SqlCommand(sql, cnc);
+                cmd.Parameters.AddWithValue("@pesquisa", pesquisa);
+
+                cnc.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+
+                        string funNome = reader.GetString(0);
+                        string funCodin = reader.GetString(1);
+                        int funReg = reader.GetInt32(2);
+                        string funSituacao = reader.GetString(3);
+
+                        lblNome.Text = funNome;
+                        lblCodin.Text = funCodin;
+                        lblReg.Text = funReg.ToString();
+                        lblSituacao.Text = funSituacao;
+
+                        if (lblSituacao.Text == "ATIVO")
+                        {
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuário desligado!", "Atenção!", MessageBoxButtons.OK);
+                            txtRegistro.Focus();
+                            txtRegistro.Clear();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Funcionário não encontrado!","Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txtRegistro.Focus();
+                        txtRegistro.Clear();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao pesquisar o funcionário: " + ex.Message);
+            }
+            finally
+            {
+                cnc.Close();
+            }
+            
+        }
+
+        private void lblCodin_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void etiqueta()
+        {
+            Console.ReadLine();
+            SqlCommand cmd = new SqlCommand("SELECT FUN_NOME, ISNULL(FUN_CODIN,0) as FUN_CODIN, FUN_REGISTRO, FUN_STATUS FROM FUNCIONARIO WHERE FUN_REGISTRO = @pesquisa;", cnc);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@pesquisa", txtRegistro.Text);
+
+            cnc.Open();
+
+            SqlDataReader dataReader = cmd.ExecuteReader();
+
+            if (dataReader.HasRows) // Verifica se há registros no dataReader
+            {
+                dataReader.Read();
+
+                string dadosImpressao = "^XA";
+                dadosImpressao += "~DG000.GRF,03072,012,";
+                dadosImpressao += ",::::::::::::::::::::::::::::O03E0,O07F0,O0HF8,N01FFC,::::O0HF8,O07F0,P080,,:::::::::::::::::::::::::::::::::L075,L07F80,L07FF4,L07FHF80,L05FIF5,M03FIFC0,N057FHF4,O01FIF,P01FHF,R0HF,R05F,Q03FF,O015FHF,O07FIF,M017FIF4,M0KF80,L07FHFD0,L07FFE80,L07FF4,L07F80,L050,,::::L0540I015,L0780J0F,L07C0I01F,L0780J0F,L07C0I01F,L0780J0F,L07D5J5F,L07FLF,::::L07C0I01F,L0780J0F,L07C0I01F,L0780J0F,L07C0I01F,L0780J0F,L010J0H1,,::::L07C,L078,L07C,L078,L07C,L078,L07C,:L07FLF,:::L07D757575,L078,L07C,L078,L07C,L078,L07C,L078,L054,,::N054,M03FE,L017FF,M0IF80,L01FHFC0,L03F9FE0,L03F07F0,L03E03E0,L07C01F0,::L07800F0,L07C01F0,L07800F0,L07C01F0,L07E2BFA2A,L07FLF,:::L0N5,,:::S01,R017,R0HF,P057FF,O01FIF,N017FHFD,N0JFE0,L017FIF,L07FHF8F80,L07FFD0F,L07F800F80,L07D0H0F,L07FE00F80,L07FFD0F,L07FHFEF80,L017FIF40,N0KF8,N015FIF,P0JF,Q07FF,R0BF,R015,,:::::::::::::::::::::::::::::::::O03E0,O07F0,O0HF8,N01FFC,::::O0HF8,O07F0,P080,,:::::::::::::::^XA";
+                dadosImpressao += "^MMT";
+                dadosImpressao += "^PW945";
+                dadosImpressao += "^LL1299";
+                dadosImpressao += "^LS0";
+                dadosImpressao += "^FT0,1088^XG000.GRF,1,1^FS";
+                dadosImpressao += "^FT135,1127^A0B,37,36^FH\\^FD" + dataReader["FUN_NOME"].ToString() + "^FS";
+                dadosImpressao += "^FT138,1255^A0B,42,40^FH\\^FDNome:^FS";
+                dadosImpressao += "^FT209,1098^A0B,42,40^FH\\^FD" + dataReader["FUN_REGISTRO"].ToString() + "^FS";
+                dadosImpressao += "^FT208,1256^A0B,42,40^FH\\^FDRegistro:^FS";
+                dadosImpressao += "^BY4,3,101^FT889,1247^B2B,,N,N";
+                dadosImpressao += "^FD" + dataReader["FUN_CODIN"].ToString() + "^FS";
+                dadosImpressao += "^PQ1,0,1,Y^XZ";
+                dadosImpressao += "^XA^ID000.GRF^FS^XZ";
+
+                //string dadosImpressao = "^XA~";
+                //dadosImpressao += "~DG000.GRF,03072,012,";
+                //dadosImpressao += ",:::::::::::T0F8,S01FC,S03FE,S07FF,::::S03FE,S01FC,T020,,:::::::::::::::::::::::::::::::::P01D40,P01FE0,P01FFD,P01FHFE0,P017FHFD40,R0KF0,R015FHFD,T07FHFC0,U07FFC0,V03FC0,V017C0,V0HFC0,T057FFC0,S01FIFC0,R05FIFD,Q03FIFE0,P01FIF4,P01FHFA0,P01FFD,P01FE0,P014,,::::P0150J0540,P01E0J03C0,P01F0J07C0,P01E0J03C0,P01F0J07C0,P01E0J03C0,P01F5J57C0,P01FLFC0,::::P01F0J07C0,P01E0J03C0,P01F0J07C0,P01E0J03C0,P01F0J07C0,P01E0J03C0,Q040J0H40,,::::P01F,P01E,P01F,P01E,P01F,P01E,P01F,:P01FLFC0,:::P01F5D5D5D40,P01E,P01F,P01E,P01F,P01E,P01F,P01E,P015,,::R015,R0HF80,Q05FFC0,Q03FFE0,Q07FHF0,Q0FE7F8,Q0FC1FC,Q0F80F8,P01F007C,::P01E003C,P01F007C,P01E003C,P01F007C,P01F8AFE8A80,P01FLFC0,:::P015L540,,:::X040,W05C0,V03FC0,T015FFC0,T07FHFC0,S05FIF40,R03FIF8,Q05FIFC0,P01FHFE3E0,P01FHF43C0,P01FE003E0,P01F4003C0,P01FF803E0,P01FHF43C0,P01FIFBE0,Q05FIFD0,R03FIFE,S057FHFC0,T03FHFC0,U01FFC0,V02FC0,W0540,,:::::::::::::::::::::::::::::::::T0F8,S01FC,S03FE,S07FF,::::S03FE,S01FC,T020,,::::::::::::::::::::::::::::::::^XA";
+                //dadosImpressao += "^MMT";
+                //dadosImpressao += "^PW945";
+                //dadosImpressao += "^LL0508";
+                //dadosImpressao += "^LS0";
+                //dadosImpressao += "^FT0,384^XG000.GRF,1,1^FS";
+                //dadosImpressao += @"^FT184,477^A0B,25,24^FH\^FD" + dataReader["FUN_NOME"].ToString() + "^FS";
+                //dadosImpressao += @"^FT306,481^A0B,25,24^FH\^FD" + dataReader["FUN_REGISTRO"].ToString() + "^FS";
+                //dadosImpressao += @"^FT132,480^A0B,33,33^FH\^FDNome:^FS";
+                //dadosImpressao += @"^FT253,479^A0B,33,33^FH\^FDRegistro:^FS";
+                //dadosImpressao += "^BY3,3,103^FT928,467^B2B,,N,N";
+                //dadosImpressao += "^FD" + dataReader["FUN_CODIN"].ToString() + "^FS";
+                //dadosImpressao += "^PQ1,0,1,Y^XZ";
+                //dadosImpressao += "^XA^ID000.GRF^FS^XZ";
+
+
+
+                string printerName = "CRACHA";
+                RawPrinterHelper.SendStringToPrinter(printerName, dadosImpressao);
+
+               //// PrintDialog printDialog = new PrintDialog();
+               // if (printDialog.ShowDialog() == DialogResult.OK)
+               // {
+               //     // Obtém a impressora selecionada pelo usuário
+               //     //string printerName = printDialog.PrinterSettings.PrinterName;
+               //     string printerName = "CRACHA";
+
+               //     // Imprime na impressora selecionada
+               //     if (RawPrinterHelper.SendStringToPrinter(printerName, dadosImpressao))
+               //     {
+               //         MessageBox.Show("Etiqueta gerada com sucesso!", "Etiqueta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               //     }
+               //     else
+               //     {
+               //         MessageBox.Show("Falha ao imprimir etiqueta", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               //     }
+               // }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum registro encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtRegistro.Focus();
+                        txtRegistro.Clear();
+            }
+
+            cnc.Close();
+            dataReader.Close();
+            cmd.Dispose();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            etiqueta();
+        }
+
+        private void txtRegistro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (txtRegistro.Text == "")
+                {
+                    MessageBox.Show("É necessário apresentar um registro", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    // Realize alguma ação caso o campo de texto esteja vazio
+
+
+
+
+                }
+
+                try
+                {
+                    string pesquisa = txtRegistro.Text;
+
+                    string sql = "SELECT FUN_NOME, isNULL(FUN_CODIN,0), FUN_REGISTRO, FUN_STATUS FROM FUNCIONARIO WHERE FUN_REGISTRO = @pesquisa";
+
+                    SqlCommand cmd = new SqlCommand(sql, cnc);
+                    cmd.Parameters.AddWithValue("@pesquisa", pesquisa);
+
+                    cnc.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+
+                            string funNome = reader.GetString(0);
+                            string funCodin = reader.GetString(1);
+                            int funReg = reader.GetInt32(2);
+                            string funSituacao = reader.GetString(3);
+
+                            lblNome.Text = funNome;
+                            lblCodin.Text = funCodin;
+                            lblReg.Text = funReg.ToString();
+                            lblSituacao.Text = funSituacao;
+
+                            if (lblSituacao.Text == "ATIVO")
+                            {
+                                
+                            }
+                            else
+                            {
+                                MessageBox.Show("Usuário desligado!", "Atenção!", MessageBoxButtons.OK);
+                                txtRegistro.Focus();
+                                txtRegistro.Clear();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Funcionário não encontrado!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            txtRegistro.Focus();
+                            txtRegistro.Clear();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao pesquisar o funcionário: " + ex.Message);
+                }
+                finally
+                {
+                    cnc.Close();
+                }
+
+            }
+        }
+    }
+    }
+    
+
